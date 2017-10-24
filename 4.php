2c4bc72281b349a8816b1d7928187276648e25cf
161710094-Ardiansyah <161710094@smkassalaambandung.sch.id>
2017-10-24 08:44:34 +0700

    bebas

---
+++ 4.php
@@ -0,0 +1,35 @@
+<?php
+    
+    require_once '3.php';
+
+    $identitas1 = new identitas("alif","bandung", "XI RPL 1","pelajar");
+    echo "nama = ".$identitas1->get_nama().'<br>';
+    echo "Tempat Lahir = ".$identitas1->get_tl().'<br>';
+    echo "kelas = ".$identitas1->get_kelas().'<br>';
+    echo "status = ".$identitas1->get_status().'<br>'.'<br>';
+
+    $identitas1 = new identitas("angga","bandung", "XI RPL 1","mahasiswa");
+    echo "nama = ".$identitas1->get_nama().'<br>';
+    echo "Tempat Lahir = ".$identitas1->get_tl().'<br>';
+    echo "kelas = ".$identitas1->get_kelas().'<br>';
+    echo "status = ".$identitas1->get_status().'<br>'.'<br>';
+
+    $identitas1 = new identitas("alif","bandung", "XI RPL 1","pelajar");
+    echo "nama = ".$identitas1->get_nama().'<br>';
+    echo "Tempat Lahir = ".$identitas1->get_tl().'<br>';
+    echo "kelas = ".$identitas1->get_kelas().'<br>';
+    echo "status = ".$identitas1->get_status().'<br>'.'<br>';
+
+    $identitas1 = new identitas("alif","bandung", "XI RPL 1","pelajar");
+    echo "nama = ".$identitas1->get_nama().'<br>';
+    echo "Tempat Lahir = ".$identitas1->get_tl().'<br>';
+    echo "kelas = ".$identitas1->get_kelas().'<br>';
+    echo "status = ".$identitas1->get_status().'<br>'.'<br>';
+
+    $identitas1 = new identitas("alif","bandung", "XI RPL 1","pelajar");
+    echo "nama = ".$identitas1->get_nama().'<br>';
+    echo "Tempat Lahir = ".$identitas1->get_tl().'<br>';
+    echo "kelas = ".$identitas1->get_kelas().'<br>';
+    echo "status = ".$identitas1->get_status().'<br>'.'<br>';
+
+    ?>
